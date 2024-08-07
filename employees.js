ea4f59c7ed614658304e5a00c78ff16b39f2dd58
Mario-Wadi <mariowadi123@hotmail.com>
2024-08-07 15:49:27 +0300

    Add files via upload

---
+++ employees.js
@@ -0,0 +1,166 @@
+export const employees = [
+    {
+        fName: "James",
+        lName: "Butt",
+        company: "Benton",
+        address: "6649 N Blue Gum St",
+        city: "New Orleans",
+        county: "Orleans",
+        color: "#8bc447",
+        photo: '../images/james-butt.jpeg'
+    },
+
+    {
+        fName: "Josephine",
+        lName: "Darakjy",
+        company: "Chanay",
+        address: "4 B Blue Ridge Blvd",
+        city: "Brighton",
+        county: "Livingston",
+        color: "#8a3b93",
+        photo: '../images/default-image.jpg'   
+    },
+
+    {
+        fName: "Art",
+        lName: "Venere",
+        company: "Chemel",
+        address: "8 W Cerritos Ave #54",
+        city: "Bridgeport",
+        county: "Gloucester",
+        color: "#1473bb",
+        photo: '../images/default-image.jpg'
+    },
+
+    {
+        fName: "Lenna",
+        lName: "Paprocki",
+        company: "Feltz Printing",
+        address: "639 Main St",
+        city: "Anchorage",
+        county: "Anchorage",
+        color: "#c32482",
+        photo: '../images/default-image.jpg'   
+    },
+
+    {
+        fName: "Donette",
+        lName: "Foller",
+        company: "Feltz Printing",
+        address: "34 Center St",
+        city: "Hamilton",
+        county: "Butler",
+        color: "#c32482",
+        photo: '../images/default-image.jpg'
+    },
+
+    {
+        fName: "Simona",
+        lName: "Morasca",
+        company: "Chanay",
+        address: "3 Mcauley DR",
+        city: "Ashland",
+        county: "Ashland",
+        color: "#8a3b93",
+        photo: '../images/default-image.jpg'
+    },
+
+    {
+        fName: "Mitsue",
+        lName: "Tollner",
+        company: "Benton",
+        address: "7 Eads St",
+        city: "Chicago",
+        county: "Cook",
+        color: "#8bc447",
+        photo: '../images/default-image.jpg'
+    },
+
+    {
+        fName: "Leota",
+        lName: "Dilliard",
+        company: "Commercial Press",
+        address: "7 W Jackson Blvd",
+        city: "San Jose",
+        county: "Santa Clara",
+        color: "#dde553",
+        photo: '../images/default-image.jpg'
+    },
+
+    {
+        fName: "Sage",
+        lName: "Wieser",
+        company: "Feltz Printing",
+        address: "5 Boston Ave #88",
+        city: "Sioux Falls",
+        county: "Minnehaha",
+        color: "#c32482",
+        photo: '../images/sage-wieser.jpeg'
+    },
+
+    {
+        fName: "Kris",
+        lName: "Marrier",
+        company: "Feltz Printing",
+        address: "228 Runamuck Pl #2808",
+        city: "Baltimore",
+        county: "Baltimore",
+        color: "#c32482",
+        photo: '../images/default-image.jpg'
+    },
+
+    {
+        fName: "Minna",
+        lName: "Amigon",
+        company: "Chanay",
+        address: "2371 Jerrold Ave",
+        city: "Kulpsville",
+        county: "Montgomery",
+        color: "#8a3b93",
+        photo: '../images/minna-amigon.jpeg'
+    },
+
+    {
+        fName: "Abel",
+        lName: "Maclead",
+        company: "Chemel",
+        address: "37275 St Rt 17m M",
+        city: "Middle Island",
+        county: "Suffolk",
+        color: "#1473bb",
+        photo: '../images/default-image.jpg'
+    },
+
+    {
+        fName: "Kiley",
+        lName: "Caldarera",
+        company: "Chemel",
+        address: "25 E 75th St #69",
+        city: "Los Angeles",
+        county: "Los Angeles",
+        color: "#1473bb",
+        photo: '../images/default-image.jpg'
+    },
+
+    {
+        fName: "Bette",
+        lName: "Ruta",
+        company: "Benton",
+        address: "98 Connecticut Ave Nw",
+        city: "Chargin Falls",
+        county: "Geauga",
+        color: "#8bc447",
+        photo: '../images/default-image.jpg'  
+    },
+
+    {
+        fName: "Veronika",
+        lName: "Albares",
+        company: "Benton",
+        address: "56 E Morehead St",
+        city: "Laredo",
+        county: "Webb",
+        color: "#8bc447",
+        photo: '../images/default-image.jpg'
+    }
+];
